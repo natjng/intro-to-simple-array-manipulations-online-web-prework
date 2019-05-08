1b6f6253214b7c8050fed55c252e586f541fea6e
Natalie J <natsetc@gmail.com>
2019-05-08 04:09:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,24 +1,59 @@
 #using_push
-
+def using_push
+  
+end
 
 #using_unshift
+def using_unshift
+  
+end
 
 #using_pop
+def using_pop
+  
+end
 
 #pop_with_args
+def pop_with_args
+  
+end
 
 #using_shift
+def using_shift
+  
+end
 
 #shift_with_args
+def shift_with_args
+  
+end
 
 #using_concat
+def using_concat
+  
+end
 
 #using_insert
+def using_insert
+  
+end
 
 #using_uniq
+def using_uniq
+  
+end
 
 #using_flatten
+def using_flatten
+  
+end
 
 #using_delete
+def using_delete
+  
+end
 
 #using_delete_at
+def using_delete_at
+  
+end
